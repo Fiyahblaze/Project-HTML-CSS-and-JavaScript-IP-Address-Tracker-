cc5c62265ebef7256053357526e75a4dfc6aa7ce
Justin <woodsj40@gmail.com>
2026-08-18 14:15:01 -0400

    eight commit

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,7 @@ import "./style.css";
 import "leaflet/dist/leaflet.css";
 
 const API_KEY = "at_8q7zetpdPKm2t2zyq2vZjfN7VTcNC";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <header class="header">
+    <h1>IP Address Tracker</h1>
