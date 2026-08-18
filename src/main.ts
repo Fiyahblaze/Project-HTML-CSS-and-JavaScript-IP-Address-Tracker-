4d1cae9b37f2b8bbdc48907546e998b8a7e46661
Justin <woodsj40@gmail.com>
2026-08-18 14:22:26 -0400

    ninth commit

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,7 @@ const API_KEY = "at_8q7zetpdPKm2t2zyq2vZjfN7VTcNC";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <header class="header">
     <h1>IP Address Tracker</h1>
+
+    document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <header class="header">
+    <h1>IP Address Tracker</h1>
