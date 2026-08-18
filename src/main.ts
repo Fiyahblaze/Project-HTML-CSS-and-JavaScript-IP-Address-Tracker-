ccdf5e9431d661bd92dda62cdf5ccb3505f21810
Justin <woodsj40@gmail.com>
2026-08-18 16:14:27 -0400

    twentyfifth commit

--- src/main.ts
+++ src/main.ts
@@ -77,8 +77,12 @@ async function getIpData(search?: string) {
   let url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`;
 
   if (search) {
+  if (search.includes(".")) {
     url += `&ipAddress=${search}`;
+  } else {
+    url += `&domain=${search}`;
   }
+}
 
   try {
   const response = await fetch(url);
